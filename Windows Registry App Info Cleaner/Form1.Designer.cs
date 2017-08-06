b9758bcacd18e8ae118c314a94280cfdf810f0e6
Realmantik <mailstome@yandex.ru>
2017-08-07 01:18:10 +0300

    Добавьте файлы проекта.

---
+++ Windows Registry App Info Cleaner/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Registry_Cleaner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Column0 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ReadMe = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(80, 86);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox1.Location = new System.Drawing.Point(12, 132);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(477, 599);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(465, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "По кнопке старт найдется всё установленное на компьютере программное обеспечение." +
+    "";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column0,
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.ReadMe});
+            this.dataGridView1.Location = new System.Drawing.Point(495, 132);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(829, 599);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(187, 86);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 40);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Clear Unusefull Information";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Column0
+            // 
+            this.Column0.HeaderText = "DisplayName";
+            this.Column0.Name = "Column0";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "URLInfoAbout";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "URLUpdateInfo";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Comments";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "HelpLink";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Contact";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "HelpTelephone";
+            this.Column6.Name = "Column6";
+            // 
+            // ReadMe
+            // 
+            this.ReadMe.HeaderText = "ReadMe";
+            this.ReadMe.Name = "ReadMe";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1336, 743);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column0;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ReadMe;
+    }
+}
+
---
+++ Windows Registry App Info Cleaner/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace Registry_Cleaner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Read(null);
+        }
+
+        public void Read(string KeyName)
+        {
+            
+            try
+            {
+                GetInstalledSoftware();                
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public void GetInstalledSoftware()
+        {
+            string displayName;
+            string uRLInfoAbout;
+            string uRLUpdateInfo;
+            string comments;
+            string helpLink;
+            string contact;
+            string helpTelephone;
+            string readMe;
+            RegistryKey key;
+            int rowsCount = 0;
+
+            // search in: CurrentUser
+            key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall");
+            foreach (String keyName in key.GetSubKeyNames())
+            {
+                RegistryKey subkey = key.OpenSubKey(keyName);
+                displayName = subkey.GetValue("DisplayName") as string;
+                uRLInfoAbout = subkey.GetValue("URLInfoAbout") as string;
+                uRLUpdateInfo = subkey.GetValue("URLUpdateInfo") as string;
+                comments = subkey.GetValue("Comments") as string;
+                helpLink = subkey.GetValue("HelpLink") as string;
+                contact = subkey.GetValue("Contact") as string;
+                helpTelephone = subkey.GetValue("HelpTelephone") as string;
+                readMe = subkey.GetValue("Readme") as string;
+
+                if (displayName!=null)
+                {
+                    dataGridView1.Rows.Add();
+                    dataGridView1.Rows[rowsCount].Cells[0].Value = displayName;
+                    dataGridView1.Rows[rowsCount].Cells[1].Value = uRLInfoAbout;
+                    dataGridView1.Rows[rowsCount].Cells[2].Value = uRLUpdateInfo;
+                    dataGridView1.Rows[rowsCount].Cells[3].Value = comments;
+                    dataGridView1.Rows[rowsCount].Cells[4].Value = helpLink;
+                    dataGridView1.Rows[rowsCount].Cells[5].Value = contact;
+                    dataGridView1.Rows[rowsCount].Cells[6].Value = helpTelephone;
+                    dataGridView1.Rows[rowsCount].Cells[7].Value = readMe;
+                    rowsCount++;
+                    textBox1.AppendText(displayName);
+                    textBox1.AppendText(Environment.NewLine);
+                }
+
+            }
+
+            // search in: LocalMachine_32
+            key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall");
+            foreach (String keyName in key.GetSubKeyNames())
+            {
+                RegistryKey subkey = key.OpenSubKey(keyName);
+                displayName = subkey.GetValue("DisplayName") as string;
+                uRLInfoAbout = subkey.GetValue("URLInfoAbout") as string;
+                uRLUpdateInfo = subkey.GetValue("URLUpdateInfo") as string;
+                comments = subkey.GetValue("Comments") as string;
+                helpLink = subkey.GetValue("HelpLink") as string;
+                contact = subkey.GetValue("Contact") as string;
+                helpTelephone = subkey.GetValue("HelpTelephone") as string;
+                readMe = subkey.GetValue("Readme") as string;
+
+                if (displayName != null)
+                {
+                    dataGridView1.Rows.Add();
+                    dataGridView1.Rows[rowsCount].Cells[0].Value = displayName;
+                    dataGridView1.Rows[rowsCount].Cells[1].Value = uRLInfoAbout;
+                    dataGridView1.Rows[rowsCount].Cells[2].Value = uRLUpdateInfo;
+                    dataGridView1.Rows[rowsCount].Cells[3].Value = comments;
+                    dataGridView1.Rows[rowsCount].Cells[4].Value = helpLink;
+                    dataGridView1.Rows[rowsCount].Cells[5].Value = contact;
+                    dataGridView1.Rows[rowsCount].Cells[6].Value = helpTelephone;
+                    dataGridView1.Rows[rowsCount].Cells[7].Value = readMe;
+                    rowsCount++;
+                    textBox1.AppendText(displayName);
+                    textBox1.AppendText(Environment.NewLine);
+                }
+            }
+
+            // search in: LocalMachine_64
+            key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall");
+            foreach (String keyName in key.GetSubKeyNames())
+            {
+                RegistryKey subkey = key.OpenSubKey(keyName);
+                displayName = subkey.GetValue("DisplayName") as string;
+                uRLInfoAbout = subkey.GetValue("URLInfoAbout") as string;
+                uRLUpdateInfo = subkey.GetValue("URLUpdateInfo") as string;
+                comments = subkey.GetValue("Comments") as string;
+                helpLink = subkey.GetValue("HelpLink") as string;
+                contact = subkey.GetValue("Contact") as string;
+                helpTelephone = subkey.GetValue("HelpTelephone") as string;
+                readMe = subkey.GetValue("Readme") as string;
+
+                if (displayName != null)
+                {
+                    dataGridView1.Rows.Add();
+                    dataGridView1.Rows[rowsCount].Cells[0].Value = displayName;
+                    dataGridView1.Rows[rowsCount].Cells[1].Value = uRLInfoAbout;
+                    dataGridView1.Rows[rowsCount].Cells[2].Value = uRLUpdateInfo;
+                    dataGridView1.Rows[rowsCount].Cells[3].Value = comments;
+                    dataGridView1.Rows[rowsCount].Cells[4].Value = helpLink;
+                    dataGridView1.Rows[rowsCount].Cells[5].Value = contact;
+                    dataGridView1.Rows[rowsCount].Cells[6].Value = helpTelephone;
+                    dataGridView1.Rows[rowsCount].Cells[7].Value = readMe;
+                    rowsCount++;
+                    textBox1.AppendText(displayName);
+                    textBox1.AppendText(Environment.NewLine);
+                }
+            }
+            key.Close();
+        }
+
+        //Ставит пустые значения для контактной информации и тому подобному
+        private void ClearUnusefullInfo()
+        {
+            string displayName;
+            RegistryKey key;
+
+            // search in: CurrentUser
+            key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall",true);
+            foreach (String keyName in key.GetSubKeyNames())
+            {
+                RegistryKey subkey = key.OpenSubKey(keyName,true);
+                displayName = subkey.GetValue("DisplayName") as string;
+                if (displayName != null)
+                {
+                    subkey.SetValue("URLInfoAbout", "");
+                    subkey.SetValue("URLUpdateInfo", "");
+                    subkey.SetValue("Comments", "");
+                    subkey.SetValue("HelpLink", "");
+                    subkey.SetValue("Contact", "");
+                    subkey.SetValue("HelpTelephone", "");
+                    subkey.SetValue("Readme", "");
+                }
+            }
+
+            // search in: LocalMachine_32
+            key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall",true);
+            foreach (String keyName in key.GetSubKeyNames())
+            {
+                RegistryKey subkey = key.OpenSubKey(keyName,true);
+                displayName = subkey.GetValue("DisplayName") as string;
+                if (displayName != null)
+                {
+                    subkey.SetValue("URLInfoAbout", "");
+                    subkey.SetValue("URLUpdateInfo", "");
+                    subkey.SetValue("Comments", "");
+                    subkey.SetValue("HelpLink", "");
+                    subkey.SetValue("Contact", "");
+                    subkey.SetValue("HelpTelephone", "");
+                    subkey.SetValue("Readme", "");
+                }
+            }
+
+            // search in: LocalMachine_64
+            key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall",true);
+            foreach (String keyName in key.GetSubKeyNames())
+            {
+                RegistryKey subkey = key.OpenSubKey(keyName, true);
+                displayName = subkey.GetValue("DisplayName") as string;
+                if (displayName != null)
+                {
+                    subkey.SetValue("URLInfoAbout", "");
+                    subkey.SetValue("URLUpdateInfo", "");
+                    subkey.SetValue("Comments", "");
+                    subkey.SetValue("HelpLink", "");
+                    subkey.SetValue("Contact", "");
+                    subkey.SetValue("HelpTelephone", "");
+                    subkey.SetValue("Readme", "");
+                }
+            }
+            key.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ClearUnusefullInfo();
+        }
+    }
+}
